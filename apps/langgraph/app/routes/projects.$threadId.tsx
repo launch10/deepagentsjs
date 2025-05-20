cc04b76fa08988c37239daccd964ee57ee552673
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-05-20 15:42:35 -0400

    Removing files from langgraph repo

--- apps/langgraph/app/routes/projects.$threadId.tsx
+++
@@ -1,8 +0,0 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/cloudflare';
-import { default as IndexRoute } from './_index';
-
-export async function loader(args: LoaderFunctionArgs) {
-  return json({ threadId: args.params.threadId });
-}
-
-export default IndexRoute;
