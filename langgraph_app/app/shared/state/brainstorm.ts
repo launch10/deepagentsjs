9e2ffb6271ff1b53c255d6dcd5f4ef1697ed57f6
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-10-14 19:40:50 -0400

    Update right quick

--- langgraph_app/app/shared/state/brainstorm.ts
+++ langgraph_app/app/shared/state/brainstorm.ts
@@ -1,6 +1,5 @@
 import type { BrainstormNextStepType, QuestionType } from "@types";
 import type { CoreGraphState } from "./core";
-
 export interface BrainstormGraphState extends CoreGraphState {
     nextQuestion: QuestionType;
     nextStep?: BrainstormNextStepType;
