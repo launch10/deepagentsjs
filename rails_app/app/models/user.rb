04cd05aaa7409e17530bea7ee4fcff104d35f74f
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-10-15 11:27:26 -0400

    Fix basic user sign-in

--- rails_app/app/models/user.rb
+++ rails_app/app/models/user.rb
@@ -69,6 +69,7 @@ class User < ApplicationRecord
   has_one :owned_account, class_name: "Account", foreign_key: "owner_id", dependent: :destroy
   has_one :payment_processor, through: :owned_account
   has_many :subscriptions, through: :owned_account
+  has_many :projects, through: :accounts
   
   def plan
     subscriptions.active.order(id: :desc).limit(1).first&.plan
