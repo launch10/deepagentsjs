b30d470f45728f931df83e20c9e5ec30bbb5244f
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-10-26 16:48:43 +0100

    WIP

--- langgraph_app/app/graphs/brainstorm.ts
+++ langgraph_app/app/graphs/brainstorm.ts
@@ -6,7 +6,15 @@ import { askQuestionNode, guardrailNode, setupNode, uiHelpNode, keepBrainstormin
 import { type LangGraphRunnableConfig } from "@langchain/langgraph";
 import { Brainstorm } from "@types";
 
-const router = (state: BrainstormGraphState, config: LangGraphRunnableConfig): string => {
+const routeAction = (state: BrainstormGraphState, config: LangGraphRunnableConfig): string => {
+    if (Brainstorm.isAction(state.action)) {
+        return state.action;
+    }
+
+    return "guardrails";
+}
+
+const routeGuardrail = (state: BrainstormGraphState, config: LangGraphRunnableConfig): string => {
     console.log(`Router received state.route: ${state.route}, isValidAnswer: ${state.isValidAnswer}`);
     
     if (!state.route) {
@@ -25,7 +33,7 @@ const router = (state: BrainstormGraphState, config: LangGraphRunnableConfig): s
 
 const seekApproval = (state: BrainstormGraphState, config: LangGraphRunnableConfig): Partial<BrainstormGraphState> => {
     return {
-        availableActions: ["FINISHED"]
+        availableActions: ["finished"]
     }
 }
 
@@ -42,13 +50,21 @@ const skip = (state: BrainstormGraphState, config: LangGraphRunnableConfig): Par
 }
 
 const helpMeAnswer = (state: BrainstormGraphState, config: LangGraphRunnableConfig): Partial<BrainstormGraphState> => {
+    // change this... actually reply (not as user, but as AIMessage), then proceed to AskQuestionNode
     return {
         isValidAnswer: false
     }
 }
 
+const doTheRest = (state: BrainstormGraphState, config: LangGraphRunnableConfig): Partial<BrainstormGraphState> => {
+    // until ACTIONS == ["FINISHED"]
+    // ask next question
+    // help me answer node
+}
+
 export const brainstormGraph = new StateGraph(BrainstormAnnotation)
     .addNode("setup", setupNode)
+    .addNode("routeAction", routeAction)
     .addNode("guardrails", guardrailNode)
     .addNode("uiHelp", uiHelpNode)
     .addNode("keepBrainstorming", keepBrainstormingNode)
@@ -57,8 +73,9 @@ export const brainstormGraph = new StateGraph(BrainstormAnnotation)
     .addNode("askQuestion", askQuestionNode)
 
     .addEdge(START, "setup")
-    .addEdge("setup", "guardrails")
-    .addConditionalEdges("guardrails", router)
+    .addEdge("setup", "routeAction")
+    .addConditionalEdges("setup", routeAction)
+    .addConditionalEdges("guardrails", routeGuardrail)
     .addEdge("uiHelp", END)
     .addEdge("keepBrainstorming", END)
     .addEdge("askQuestion", END)
