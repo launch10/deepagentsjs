802acb01130a104ecfeed22c0d6a7429ad889989
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-11-05 17:07:14 -0500

    Simple

--- langgraph_app/app/core/graphParams.ts
+++ langgraph_app/app/core/graphParams.ts
@@ -1,14 +1,7 @@
 import { PostgresSaver } from "@langchain/langgraph-checkpoint-postgres";
 import { pool } from "./postgres";
-import { env } from "./env";
 
-// Singleton instances
-let checkpointer: PostgresSaver | null = null;
-
-// We only setup checkpointer if we're not running inside Langgraph server
-if (env.LANGGRAPH_SERVER !== 'true') {
-    checkpointer = new PostgresSaver(pool);
-    await checkpointer.setup(); // Run migrations only once
-}
+const checkpointer = new PostgresSaver(pool);
+await checkpointer.setup();
 
 export const graphParams = { checkpointer };
