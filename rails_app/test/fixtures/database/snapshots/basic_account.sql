abd113791310ea533a2ad86e105cfb3bdf469613
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-11-20 11:42:30 -0500

    Update snapshot to include theme_type etc

--- rails_app/test/fixtures/database/snapshots/basic_account.sql
+++ rails_app/test/fixtures/database/snapshots/basic_account.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:720b974c0e496f0940ca21cc41ab73ba82ad78aaef547b8f080192cfe037eccc
-size 33155635
+oid sha256:8bd7bcce1d8015c30c5f24fa53d35b73d7b23545b81e8f40a99a2b27a895ca1b
+size 33160293
