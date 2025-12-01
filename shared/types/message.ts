4f967ae8ad794c34fa4d55f852ca120623ec34cf
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-12-01 09:12:40 -0500

    Update recorded snapshots

--- shared/types/message.ts
+++ shared/types/message.ts
@@ -2,7 +2,7 @@ import type { LangGraphRunnableConfig } from "@langchain/langgraph";
 import { type BaseMessageLike, BaseMessage, AIMessage, HumanMessage, SystemMessage, type Message } from "@langchain/core/messages";
 export type { BaseMessageLike, BaseMessage, AIMessage, HumanMessage, SystemMessage, Message, LangGraphRunnableConfig };
 interface RecordWithMessages {
-    messages: BaseMessage[];
+  messages: BaseMessage[];
 }
 
 export const firstHumanMessage = (record: RecordWithMessages): HumanMessage | undefined => {
