63fc70159911fb1b4cf23fd96bad85d06a379e1e
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-12-03 09:17:23 -0500

    Simplify controller

--- rails_app/app/controllers/projects_controller.rb
+++ rails_app/app/controllers/projects_controller.rb
@@ -8,7 +8,7 @@ class ProjectsController < SubscribedController
   end
 
   def show
-    project = Project.with_launch_relations.find_by!(account_id: current_account.id, uuid: params[:uuid])
+    project = current_account.projects.find_by(uuid: params[:uuid])
 
     if !project
       render json: { error: "Project not found" }, status: :not_found
