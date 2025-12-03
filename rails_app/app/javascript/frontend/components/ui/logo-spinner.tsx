ff69336cce20a12cf08e509d95e76c9d14235020
Becca Roth <rothmation@gmail.com>
2025-12-02 16:30:25 -0800

    Lottie Animation - Installed `dotlottie-react` package - Setup LogoSpinner component and story

---
+++ rails_app/app/javascript/frontend/components/ui/logo-spinner.tsx
@@ -0,0 +1,14 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { Rocket } from "lucide-react";
+
+export default function LogoSpinner() {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/0ca8bd09-c6ca-4d89-8fc8-325ab732e678/0wCnXtYuFr.lottie"
+      loop
+      autoplay
+      height={42}
+      width={42}
+    />
+  );
+}
