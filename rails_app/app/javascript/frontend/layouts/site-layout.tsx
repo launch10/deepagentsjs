f907f0d1f63b2781f2028a149ef20dac2e504585
Becca Roth <rothmation@gmail.com>
2025-12-11 10:33:49 -0800

    Clean up WorkflowProgressContext - Use `Workflow` from `@shared` - Remove unnecessary import in `Header.tsx` - Get workflow steps from pageProps

--- rails_app/app/javascript/frontend/layouts/site-layout.tsx
+++ rails_app/app/javascript/frontend/layouts/site-layout.tsx
@@ -1,5 +1,4 @@
 import Header from "@components/header/header";
-import { workflow } from "@shared";
 import { WorkflowProgressProvider } from "@contexts/workflow-progress-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -8,7 +7,7 @@ const queryClient = new QueryClient();
 export const SiteLayout = ({ children }: { children: React.ReactNode }): React.ReactNode => {
   return (
     <QueryClientProvider client={queryClient}>
-      <WorkflowProgressProvider steps={workflow.launch.steps}>
+      <WorkflowProgressProvider>
         <div className="bg-background min-h-screen">
           <Header />
           {children}
