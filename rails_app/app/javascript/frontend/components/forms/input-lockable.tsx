7404c5be5f2a0a7afb9b0b6a5123e57efe88d061
Becca Roth <rothmation@gmail.com>
2025-12-15 16:34:33 -0800

    Improved logic on InputLockable

--- rails_app/app/javascript/frontend/components/forms/input-lockable.tsx
+++ rails_app/app/javascript/frontend/components/forms/input-lockable.tsx
@@ -1,6 +1,6 @@
-import { Lock, LockOpen } from "lucide-react";
-import { InputGroup, InputGroupInput } from "@components/ui/input-group";
 import { Button } from "@components/ui/button";
+import { InputGroup, InputGroupInput } from "@components/ui/input-group";
+import { Lock, LockOpen, Trash2 } from "lucide-react";
 import { forwardRef } from "react";
 
 interface InputLockableProps {
@@ -10,23 +10,40 @@ interface InputLockableProps {
   isInvalid?: boolean;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onLockToggle?: () => void;
+  onDelete?: () => void;
   name?: string;
 }
 
 const InputLockable = forwardRef<HTMLInputElement, InputLockableProps>(
   (
-    { placeholder, value, isLocked = false, isInvalid = false, onChange, onLockToggle, name },
+    {
+      placeholder,
+      value,
+      isLocked = false,
+      isInvalid = false,
+      onChange,
+      onLockToggle,
+      onDelete,
+      name,
+    },
     ref
   ) => {
     return (
       <div className="flex items-center gap-1">
-        <Button type="button" onClick={onLockToggle} variant="ghost" size="icon" className="p-0 h-auto w-auto">
-          {isLocked ? (
-            <Lock size={12} className="text-base" />
-          ) : (
-            <LockOpen size={12} className="text-[#96989B]" />
-          )}
-        </Button>
+        {onLockToggle ? (
+          <Button type="button" onClick={onLockToggle} variant="ghost" size="icon">
+            {isLocked ? (
+              <Lock size={12} className="text-base" />
+            ) : (
+              <LockOpen size={12} className="text-[#96989B]" />
+            )}
+          </Button>
+        ) : null}
+        {onDelete ? (
+          <Button type="button" onClick={onDelete} variant="ghost" size="icon">
+            <Trash2 size={14} className="text-[#96989B]" />
+          </Button>
+        ) : null}
         <InputGroup className={`h-10 ${isLocked ? "border-[#2E3238]" : ""}`}>
           <InputGroupInput
             ref={ref}
