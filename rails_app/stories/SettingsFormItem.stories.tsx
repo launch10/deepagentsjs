e3eac743b977d875f4f11b126ec972864a63d756
Becca Roth <rothmation@gmail.com>
2025-12-18 10:18:01 -0800

    Add stories for KeywordsForm and SettingsForm

---
+++ rails_app/stories/SettingsFormItem.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SettingsFormItem from "~/components/ads/Forms/SettingsForm/LocationTargetingItem";
+import { FormProvider, useForm } from "react-hook-form";
+import {
+  settingsFormSchema,
+  type SettingsFormData,
+} from "~/components/ads/Forms/SettingsForm/settingsForm.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const meta = {
+  title: "Ad Campaign/Settings Form Item",
+  component: SettingsFormItem,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    location: {
+      criteria_id: 9060323,
+      name: "Historic Core",
+      canonical_name: "Historic Core,California,United States",
+      target_type: "Neighborhood",
+      country_code: "US",
+      radius: 11,
+      isTargeted: false,
+      id: "f82b7b6b-f493-4236-aebf-3ccc0ffd9662",
+    },
+    index: 0,
+    handleRemoveLocation: () => {},
+    handleToggleTargeted: () => {},
+  },
+  globals: {
+    backgrounds: {
+      default: "background",
+    },
+  },
+  decorators: [
+    (Story) => {
+      const queryClient = new QueryClient();
+      const methods = useForm<SettingsFormData>({
+        resolver: zodResolver(settingsFormSchema) as any,
+        mode: "onChange",
+        defaultValues: {
+          settings: [],
+        },
+      });
+      return (
+        <QueryClientProvider client={queryClient}>
+          <FormProvider {...methods}>
+            <Story />
+          </FormProvider>
+        </QueryClientProvider>
+      );
+    },
+  ],
+} satisfies Meta<typeof SettingsFormItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
