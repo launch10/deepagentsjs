ad640f7e963099a77f436045ced2107cb2eab3ad
Becca Roth <rothmation@gmail.com>
2025-12-18 10:23:11 -0800

    AdsChat fixes - Max height for long chats - ReactMarkdown for bot messages

--- rails_app/app/javascript/frontend/components/ads/Sidebar/AdsChat.tsx
+++ rails_app/app/javascript/frontend/components/ads/Sidebar/AdsChat.tsx
@@ -4,8 +4,8 @@ import AdsChatInput from "./AdsChat/AdsChatInput";
 
 export default function AdsChat() {
   return (
-    <div className="bg-background rounded-b-2xl min-h-[300px] flex flex-col">
-      <CardContent className="flex-1 overflow-y-auto px-4 py-4">
+    <div className="bg-background rounded-b-2xl  flex flex-col">
+      <CardContent className="flex-1 overflow-y-auto px-4 py-4 max-h-[300px]">
         <AdsChatMessages />
       </CardContent>
       <CardFooter className="flex-col gap-1 px-4 pb-4 pt-0 items-start">
