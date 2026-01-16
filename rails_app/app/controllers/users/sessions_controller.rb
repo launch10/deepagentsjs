54792b43083c793458d2392ec38e177684f7addd
Brett Shollenberger <brett.shollenberger@gmail.com>
2026-01-16 10:25:59 -0500

    Fix warden devise jwt logout

--- rails_app/app/controllers/users/sessions_controller.rb
+++ rails_app/app/controllers/users/sessions_controller.rb
@@ -3,7 +3,6 @@ class Users::SessionsController < Devise::SessionsController
 
   # We need to intercept the Sessions#create action for processing OTP
   prepend_before_action :authenticate_with_two_factor, only: [:create]
-
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_user_session_path, alert: "Try again later." }
 
   def authenticate_with_two_factor
